e78f57168ca6dc9e735d26afed7f83f9e98756c0
magiccoder890 <23fzhu@athenian.org>
2020-09-09 14:46:24 +0000

    This is Bert's new homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,29 +2,65 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Calculates the median of the three inputs
-    public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+    public static int median(int a,int b, int c) {
+        int sum = a + b + c;
+        int max_number_firsttwo = Math.max(a,b);
+        int max_number = Math.max(max_number_firsttwo,c);
+        int min_number_firsttwo = Math.min(a,b);
+        int min_number = Math.min(min_number_firsttwo,c);
+        int median_number = sum - max_number - min_number;
+        System.out.println(median_number);
+        return median_number;
     }
 
-    // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        int ab_a = Math.abs(a);
+        int ab_b = Math.abs(b);
+        if (Math.max(ab_a, ab_b) == ab_a) {
+            System.out.println(a);
+        }
+        else {
+            System.out.println(b);
+        }
+        return ab_a;
     }
 
-    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
-    // where "a" and "b" are the inputs to the method
-    public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+    public static int pythagoras(int a, int b) {
+        int c_sq = (int) (Math.pow((a+b),2) - 2*a*b);
+        System.out.println(Math.sqrt(c_sq));
+        return c_sq;
     }
 
-    public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
+    public static void main(String[] arg){
 
-        Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What command would you like to carry out");
+        String command = scanner.next();
+        if (command.equals("median")) {
+            System.out.print("What's the first number");
+            int a = scanner.nextInt();
+            System.out.print("What's the second number");
+            int b = scanner.nextInt();
+            System.out.print("What's the third number");
+            int c= scanner.nextInt();
+            median(a, b, c);
+        }
+        else if (command.equals("magnitude")) {
+            System.out.print("What's the first number");
+            int a = scanner.nextInt();
+            System.out.print("What's the second number");
+            int b = scanner.nextInt();
+            magnitude(a , b);
+        }
+        else if (command.equals("pythagoras")){
+            System.out.print("What's the first number");
+            int a = scanner.nextInt();
+            System.out.print("What's the second number");
+            int b = scanner.nextInt();
+            pythagoras(a, b);
+        }
+        else {
+            System.out.println("There are some errors:)");
+        }
     }
 }
